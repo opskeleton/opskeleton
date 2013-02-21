9c1078ede6cde3cf794e00cd456f55961de5a3cc
narkisr <narkisr@gmail.com>
2013-02-21 06:24:57 +0200

    clearing default manifest

--- templates/scripts/run.sh
+++ templates/scripts/run.sh
@@ -1,2 +1,1 @@
-puppet apply --modulepath=modules:static-modules manifests/default.pp\
-  --node_terminus exec --external_nodes=`pwd`/scripts/lookup.sh --hiera_config hiera.yaml $@
+puppet apply --modulepath=modules:static-modules --node_terminus exec --external_nodes=`pwd`/scripts/lookup.sh --hiera_config hiera.yaml $@
