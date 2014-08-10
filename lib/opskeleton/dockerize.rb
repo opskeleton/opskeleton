33c56fce08aa56a31474be1ccc86eff7f9751774
ronen <narkisr@gmail.com>
2014-08-10 12:26:33 +0300

    template by os type

--- lib/opskeleton/dockerize.rb
+++ lib/opskeleton/dockerize.rb
@@ -2,11 +2,14 @@ module Opsk
   class Dockerize < Thor::Group
     include Thorable, Thor::Actions
 
+    argument :from, :type => :string, :desc => 'which source image to use'
+    argument :os_type, :type=> :string, :desc => 'Ubuntu/Centos'
+
+
     desc 'Creates a docker image using the current opsk sandbox'
 
     def create_dockerfile
-	empty_directory('docker')
-	template("templates/#{type_of}/docker.erb", "docker/Dockerfile")
+	template("templates/#{type_of}/#{os_type}_docker.erb", 'Dockerfile')
     end
 
   end
