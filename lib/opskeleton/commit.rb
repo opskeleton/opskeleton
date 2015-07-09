49edae072e2198793a4d013b821e7be6dae6dd36
ronen <narkisr@gmail.com>
2015-07-09 16:43:46 +0300

    adding all and clearing writable

--- lib/opskeleton/commit.rb
+++ lib/opskeleton/commit.rb
@@ -1,18 +1,10 @@
 
-def add_writable(g,with)
-  readonly = g.remotes.first.url
-  writable = readonly.gsub(/git\:\/\/*\//,with)
-  unless readonly.eql?(writable)
-    g.add_remote('writable',writable) 
-  end
-end
-
 module  Opsk
   class Commit < Thor::Group
     include Thorable, Thor::Actions
 
     class_option :message, :type=> :string, :desc => 'optional commit message'
-    class_option :writable_remote, :type=> :string, :desc => 'add remote write repo', :default => 'git@'
+    class_option :all, :type=> :boolean, :desc => 'commit all', :default => false
 
     def validate
 	check_root
@@ -24,16 +16,18 @@ module  Opsk
 	  if File.exists?("#{d}/.git")
 	    g = Git.init(d)
 	    if g.status.changed.keys.length > 0
-		puts "Changes found for #{d}:\n\n"
-		puts "#{g.show}\n"
-		g.checkout('master')
-		if options['message']
-		  g.commit_all(options['message']) 
-		else 
-		  puts 'Please provide commit message:'
-		  g.commit_all(STDIN.gets.chomp) 
+		puts "Listing changes for #{d}:\n\n"
+		puts "#{g.show}\n\n"
+		puts "Commit the changes under #{d}? (y/n)\n\n" unless options['all']
+		if(options['all'] or STDIN.gets.chomp.eql?('y'))
+		  g.checkout('master')
+		  if options['message']
+		    g.commit_all(options['message']) 
+		  else 
+		    puts 'Please provide commit message:\n'
+		    g.commit_all(STDIN.gets.chomp) 
+		  end
 		end
-		add_writable(g,options['writable_remote']) if options['writable_remote']
 	    end
 	  end
 	end
