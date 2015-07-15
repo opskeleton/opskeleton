4a702175a8affd2fa29ff1ffab02934e7b441b32
ronen <narkisr@gmail.com>
2015-07-15 17:21:24 +0300

    extracting logic to a different class

--- lib/opskeleton/commit.rb
+++ lib/opskeleton/commit.rb
@@ -1,14 +1,42 @@
 
-def report(g)
-  %i(changed added untracked).each do |state|
-    say "#{state} files:\n\n"
-    g.status.send(state).each do |k,v|
-	say "- #{k}"
-    end
-  end
-end
 
 module  Opsk
+  class CommitGit
+    def initialize(d,options,thor)
+	@g = Git.init(d)
+	@options = options
+	@thor = thor
+    end
+
+    def changed?
+	@g.status.changed.keys.length > 0
+    end
+
+    def report
+	%i(changed added untracked).each do |state|
+	  @thor.say "#{state} files:\n\n"
+	  @g.status.send(state).each do |k,v|
+	    @thor.say "- #{k}"
+	  end
+	  @thor.say "\n"
+	end
+    end
+
+    def master_commit(d)
+	resp = @thor.yes? "Commit the changes under #{d}? (y/n)" unless @options['all']
+	if(@options['all'] or resp)
+	  @g.checkout('master')
+	  if @options['message']
+	    @g.commit_all(@options['message']) 
+	  else 
+	    @thor.say 'Commit message:'
+	    @g.commit_all(STDIN.gets.chomp) 
+	  end
+	end
+
+    end
+  end
+
   class Commit < Thor::Group
     include Thorable, Thor::Actions
 
@@ -23,25 +51,19 @@ module  Opsk
     def commit
 	Dir["modules/*"].reject{|o| not File.directory?(o)}.each do |d|
 	  if File.exists?("#{d}/.git")
-	    g = Git.init(d)
-	    if g.status.changed.keys.length > 0
-		say "Listing changes for #{d}:\n\n"
-		report(g)
-		resp = yes? "Commit the changes under #{d}? (y/n)" unless options['all']
-		if(options['all'] or resp)
-		  g.checkout('master')
-		  if options['message']
-		    g.commit_all(options['message']) 
-		  else 
-		    say 'Commit message:'
-		    g.commit_all(STDIN.gets.chomp) 
-		  end
+	    begin
+		cg = Opsk::CommitGit.new(d,options,self)
+		if cg.changed?
+		  say "Listing changes for #{d}:\n\n"
+		  cg.report
+		  cg.master_commit(d)
 		end
+	    rescue => e 
+		say "Failed to commit #{d} due to #{e}"
 	    end
 	  end
 	end
     end
-
 
   end
 end
