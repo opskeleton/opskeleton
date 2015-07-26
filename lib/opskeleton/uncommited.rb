09bf1aedfc7d9f52f40be8d76fe3ff925c66e295
ronen <narkisr@gmail.com>
2015-07-26 18:06:18 +0300

    remove dead code

--- lib/opskeleton/uncommited.rb
+++ lib/opskeleton/uncommited.rb
@@ -1,49 +1,12 @@
 
 
 module  Opsk
-  class CommitGit
-    def initialize(d,options,thor)
-	@g = Git.init(d)
-	@options = options
-	@thor = thor
-    end
-
-    def changed?
-	@g.status.changed.keys.length > 0
-    end
-
-    def report
-	%i(changed added untracked).each do |state|
-	  @thor.say "#{state} files:\n\n"
-	  @g.status.send(state).each do |k,v|
-	    @thor.say "- #{k}"
-	  end
-	  @thor.say "\n"
-	end
-    end
-
-    def master_commit(d)
-	resp = @thor.yes? "Commit the changes under #{d}? (y/n)" unless @options['all']
-	if(@options['all'] or resp)
-	  @g.checkout('master')
-	  if @options['message']
-	    @g.commit_all(@options['message']) 
-	  else 
-	    @thor.say 'Commit message:'
-	    @g.commit_all(STDIN.gets.chomp) 
-	  end
-	end
-
-    end
-  end
-
   class Uncommited < Thor::Group
     include Thorable, Thor::Actions
 
     def validate
 	check_root
     end
-
 
     def uncommited
 	Dir["modules/*"].reject{|o| not File.directory?(o)}.each do |d|
